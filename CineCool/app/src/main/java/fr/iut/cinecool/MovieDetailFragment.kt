9840677f9efb81bfd79c3a42cab642e116c5803b
louwar <loup6378@gmail.com>
2023-03-31 10:03:08 +0200

    Update Detail

--- CineCool/app/src/main/java/fr/iut/cinecool/MovieDetailFragment.kt
+++ CineCool/app/src/main/java/fr/iut/cinecool/MovieDetailFragment.kt
@@ -1,75 +1,53 @@
 package fr.iut.cinecool
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import fr.iut.cinecool.API.Movie
+// import fr.iut.cinecool.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment() {
-/*
-    private lateinit var binding: FragmentMovieDetailBinding
+    /*private var _binding: FragmentDetailMovieBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?, savedInstanceState: Bundle?): View
-    {
-        binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailMovieBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val movieId = arguments?.getInt(ARG_MOVIE_ID)
+        // Récupérez le film passé en argument
+        val movie: Movie? = arguments?.getParcelable("movie")
 
-        // Appel de l'API pour récupérer les détails du film
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = movieId?.let { MovieApiService().getMovieDetails(it) }
-            withContext(Dispatchers.Main)
-            {
-                if (response != null) {
-                    if (response.isSuccessful)
-                    {
-                        val movie = response.body()
-                        if (movie != null)
-                        {
-                            // Affichage des détails du film dans l'interface utilisateur
-                            binding.titreFilm.text = movie.title
-                            binding.description.text = movie.overview
-                            binding.afficheFilm.load("https://image.tmdb.org/t/p/w500${movie.posterPath}") // Utilisation de la librairie Coil pour charger l'image
-                        }
-                    } else
-                    {
-                        Toast.makeText(
-                            requireContext(),
-                            getString(R.string.error_loading_movie),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-            }
+        // Mettez à jour les vues avec les données du film
+        if (movie != null) {
+            binding.titreFilm.text = movie.title
+            binding.description.text = movie.overview
+
+            val imageUrl = "https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            Glide.with(binding.afficheFilm.context)
+                .load(imageUrl)
+                .placeholder(R.drawable.imitation_game)
+                .into(binding.afficheFilm)
         }
-        // Ajout d'un écouteur de clic pour le bouton de retour
+
+        // Gérer le clic sur le bouton de retour
         binding.backButton.setOnClickListener {
-            requireActivity().onBackPressed()
+            findNavController().popBackStack()
         }
     }
 
-    companion object {
-        private const val ARG_MOVIE_ID = "movie_id"
-
-        fun newInstance(movieId: Int): MovieDetailFragment {
-            val args = Bundle().apply {
-                putInt(ARG_MOVIE_ID, movieId)
-            }
-            return MovieDetailFragment().apply {
-                arguments = args
-            }
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }*/
 }
+
